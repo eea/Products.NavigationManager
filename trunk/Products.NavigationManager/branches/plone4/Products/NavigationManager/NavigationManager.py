90215724cb6a4b7fa425e0176ef2f2bc2e961dc5
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-08-08 16:25:45 +0000

    Refs #8361   - Cleanup deprecated overrides selectViewTemplate and saveDefaultPage used by 'is_empty' index.   - Moved portlet_navigation_sections from eea.themecentre;   - Deleted NavigationManager_public skins layer.

--- trunk/Products.NavigationManager/branches/plone4/Products/NavigationManager/NavigationManager.py
+++ trunk/Products.NavigationManager/branches/plone4/Products/NavigationManager/NavigationManager.py
@@ -2,13 +2,8 @@
 """
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.utils import UniqueObject
-from Products.CMFPlone import PloneMessageFactory as _
-from Products.CMFDynamicViewFTI.interfaces import ISelectableBrowserDefault
 from Products.Archetypes.public import BaseFolder, BaseFolderSchema
-from Products.statusmessages.interfaces import IStatusMessage
 
-from zope.event import notify
-from zope.lifecycleevent import ObjectModifiedEvent
 
 class NavigationManager(UniqueObject, BaseFolder):
     """ Navigation manager
@@ -20,59 +15,11 @@ class NavigationManager(UniqueObject, BaseFolder):
     def __init__(self, oid='portal_navigationmanager'):
         super(NavigationManager, self).__init__(oid=oid)
 
-    # Methods
-    def selectViewTemplate(self, context, templateId):
-        """ Helper method to select a view template.
-
-        Returns action status
-        """
-        if not ISelectableBrowserDefault.providedBy(context):
-            msg = _(u'Object does not support selecting layout templates')
-            IStatusMessage(context.REQUEST).addStatusMessage(msg, 'error')
-            return 'failure'
-
-        context.setLayout(templateId)
-        notify(ObjectModifiedEvent(context))
-
-        msg = _(u'View changed.')
-        IStatusMessage(context.REQUEST).addStatusMessage(msg, 'info')
-        return 'success'
-
-    def saveDefaultPage(self, context, objectId=None):
-        """ Helper method to select a default page for a folder view.
-
-        Returns action status
-        """
-        if not objectId:
-            msg = _(u'Please select an item to use.')
-            IStatusMessage(context.REQUEST).addStatusMessage(msg, 'error')
-            return 'missing'
-
-        if not ISelectableBrowserDefault.providedBy(context):
-            raise NotImplementedError(
-                "Object does not support setting default page")
-
-        # Also should never happen
-        if objectId not in context.objectIds():
-            msg = _(
-                u'There is no object with short name ${name} in this folder.',
-                mapping={u'name' : objectId})
-
-            IStatusMessage(context.REQUEST).addStatusMessage(msg, 'error')
-            return 'failure'
-
-        context.setDefaultPage(objectId)
-        notify(ObjectModifiedEvent(context))
-
-        msg = _(u'View changed.')
-        IStatusMessage(context.REQUEST).addStatusMessage(msg, 'info')
-        return 'success'
-
     def getTree(self, site, tabselected='default', recursive=True):
+        """ It returns a list  of menu items objects from the root of this
+            menu manager. Useful to generate top navigation like portal tabs.
         """
-        It returns a list  of menu items objects from the root of this menu manager.
-        Useful to generate top navigation like portal tabs.
-        """
+
         portal = getToolByName(self, 'portal_url').getPortalObject()
         request = self.REQUEST
         actualUrl = request.get('ACTUAL_URL')
