ee9492217a86129a99116650f089faf3a369f742
eduard-fironda <eduard.fironda@eaudeweb.ro>
2017-04-20 14:04:53 +0300

    updated package information
    * Change: updated package information
      [eduard-fironda]

--- setup.py
+++ setup.py
@@ -13,15 +13,24 @@ setup(
     description="EEA Navigation Manager skin for EEA",
     long_description=open("README.rst").read() + "\n" +
                      open(os.path.join("docs", "HISTORY.txt")).read(),
-    url="https://svn.eionet.europa.eu/projects/"
-        "Zope/browser/trunk/Products.NavigationManager",
-    classifiers=[
-      "Framework :: Plone",
-      "Programming Language :: Python",
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Framework :: Zope2",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Programming Language :: Zope",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
       ],
-    keywords='EEA Products NavigationManager',
-    author='Antonio de Marinis (EEA), European Environment Agency',
-    author_email='webadmin@eea.europa.eu',
+    keywords='EEA Products NavigationManager Add-ons Plone Zope',
+    author='European Environment Agency: IDM2 A-Team',
+    author_email='eea-edw-a-team-alerts@googlegroups.com',
+    url='https://github.com/eea/Products.NavigationManager',
     license='GPL',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['Products'],
