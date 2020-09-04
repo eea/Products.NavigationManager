0db39a02a2b07c066115e1727f5df5f2dbfa3abc
EEA Jenkins <eea-github@googlegroups.com>
2020-09-04 16:17:39 +0300

    Updated setup.py, added long_description_content_type - needs review

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     name=NAME,
     version=VERSION,
     description="EEA Navigation Manager skin for EEA",
+    long_description_content_type="text/x-rst",
     long_description=open("README.rst").read() + "\n" +
                      open(os.path.join("docs", "HISTORY.txt")).read(),
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
